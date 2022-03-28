80f65ebe3498796731c1836618dde0227496f44c
Pavankolhe27 <101618136+Pavankolhe27@users.noreply.github.com>
2022-03-28 11:47:37 +0530

    Add files via upload

---
+++ javaworld.java
@@ -0,0 +1,17 @@
+package Self_EvaluationLab1;
+
+public class javaworld {
+	public static void main(String[] args)
+	{
+
+	for(int i=0;i<=15; i++)
+	{
+		System.out.println("JAVAWORLD");
+		
+	}
+
+}
+
+}
+
+
